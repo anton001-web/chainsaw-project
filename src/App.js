bc4196e91a47e792a031b8e5449f0a6c87560812
Anton <antonreva846@gmail.com>
2022-10-30 21:40:25 +0200

    webpack assembly, project-hierarchy was added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles/main.scss'
+import Test from "./components/Test";
+
+const App = () => {
+    return (
+        <>
+            <Test />
+        </>
+    )
+}
+
+export default App
